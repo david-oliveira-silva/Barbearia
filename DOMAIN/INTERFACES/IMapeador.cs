68e7aaacf518e1ef0441e42e492b9729f3c5e5a1
david-oliveira-silva <luceni0122@gmail.com>
2026-02-16 15:01:06 -0300

    Add project files.

---
+++ DOMAIN/INTERFACES/IMapeador.cs
@@ -0,0 +1,10 @@
+﻿namespace DOMAIN.INTERFACES
+{
+    public interface IMapeador<T>
+    {
+        void Cadastrar(T item);
+        void Atualizar(T item);
+        void Excluir(T item);
+            IEnumerable<T> ObterTodos();
+    }
+}
---
+++ DOMAIN/MODELS/ClienteModel.cs
@@ -0,0 +1,20 @@
+﻿namespace DOMAIN.MODELS
+{
+    public class ClienteModel
+    {
+        public string? Nome { get; set; }
+        public string? Cpf { get; set; }
+        public string? Email { get; set; }
+        public string? Telefone { get; set; }
+
+        public ClienteModel() { }
+
+        public ClienteModel(string? nome, string? cpf, string? email, string? telefone)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Email = email;
+            Telefone = telefone;
+        }
+    }
+}
